da36d074ec972cc5205186c468d8ab4865986f53
Limbureuben <limbureuben4@gmail.com>
2025-05-23 16:15:38 +0300

    changes

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 import 'bootstrap/dist/js/bootstrap.bundle'
 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js' // This is CRITICAL for dropdowns!
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
 createApp(App)
   .use(router)
   .use(MotionPlugin)
