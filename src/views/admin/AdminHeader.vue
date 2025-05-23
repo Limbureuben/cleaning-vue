140a5824f008aa2d57f424a7644bc38d6fc46e98
Limbureuben <limbureuben4@gmail.com>
2025-05-23 16:00:55 +0300

    changes

--- src/views/admin/AdminHeader.vue
+++ src/views/admin/AdminHeader.vue
@@ -1,15 +1,15 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark shadow" style="background-color: #FE4F2D">
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-top shadow" style="background-color: #FE4F2D;">
     <div class="container-fluid">
       <!-- Brand -->
       <router-link to="/" class="navbar-brand fw-bold">Admin Dashboard</router-link>
 
-      <!-- Toggler for small devices -->
+      <!-- Toggler for small screens -->
       <button class="navbar-toggler" type="button" @click="toggleMenu">
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      <!-- Navbar links -->
+      <!-- Collapsible content -->
       <div :class="['collapse navbar-collapse', { show: isMenuOpen }]">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
@@ -23,8 +23,7 @@
           </li>
         </ul>
 
-        <!-- Logout button -->
-        <button class="btn btn-outline-light ms-auto" @click="logout">
+        <button class="btn btn-outline-light" @click="logout">
           <i class="bi bi-box-arrow-right me-1"></i> Logout
         </button>
       </div>
@@ -50,11 +49,15 @@ const logout = () => {
 </script>
 
 <style scoped>
+.navbar {
+  z-index: 1050; /* ensure it stays on top */
+}
+
 .navbar-brand {
   font-size: 1.3rem;
 }
 
-.navbar-nav .nav-link {
+.nav-link {
   font-weight: 500;
 }
 
