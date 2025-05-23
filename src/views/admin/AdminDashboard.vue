685b2c2d01aab7263b4b70eac61bc7e3735dc12a
Limbureuben <limbureuben4@gmail.com>
2025-05-23 16:29:30 +0300

    changes

--- src/views/admin/AdminDashboard.vue
+++ src/views/admin/AdminDashboard.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="container mt-4">
-    <h2 class="text-center mb-4">Admin Dashboard</h2>
-
     <div class="row">
       <div class="col-md-6 mb-4" v-for="(card, index) in cards" :key="index">
         <div class="card shadow border-0 h-100">
