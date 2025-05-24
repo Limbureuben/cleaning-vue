d1eb552df8b20d128881c76f29681a30cd618a63
Limbureuben <limbureuben4@gmail.com>
2025-05-24 13:02:39 +0300

    changes

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,13 +3,15 @@ import HomepageVue from '@/views/Homepage.vue'
 import Register from '@/sharing/Register.vue'
 import Login from '@/sharing/Login.vue'
 import AdminDashboard from '@/views/admin/AdminDashboard.vue'
+import UserHeader from '@/views/user/UserHeader.vue'
 
 
 const routes = [
   { path: '/', name: 'Homepage', component: HomepageVue },
   { path: '/register', name: 'Register', component: Register },
   { path: '/login', name: 'Login', component: Login },
-  { path: '/admin-dashboard', name: 'AdminDashboard', component: AdminDashboard }
+  { path: '/admin-dashboard', name: 'AdminDashboard', component: AdminDashboard },
+  { path: '/user', name: 'UserDashboard', component: UserHeader}
   
 ]
 const router = createRouter({
