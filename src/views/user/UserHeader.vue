b89f0c523935a28c6f2e506458104cc39f864213
Limbureuben <limbureuben4@gmail.com>
2025-05-24 10:52:23 +0300

    changes

--- src/views/user/UserHeader.vue
+++ src/views/user/UserHeader.vue
@@ -1,19 +1,16 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark fixed-top shadow" style="background-color: #FE4F2D;">
     <div class="container-fluid">
-      <!-- Brand -->
       <router-link to="/" class="navbar-brand fw-bold">User Dashboard</router-link>
 
-      <!-- Toggler for small screens -->
       <button class="navbar-toggler" type="button" @click="toggleMenu">
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      <!-- Collapsible content -->
       <div :class="['collapse navbar-collapse', { show: isMenuOpen }]">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <router-link to="/admin/dashboard" class="nav-link">Companys</router-link>
+            <router-link to="/admin/dashboard" class="nav-link">Companies</router-link>
           </li>
           <li class="nav-item">
             <router-link to="/admin/users" class="nav-link">Users</router-link>
