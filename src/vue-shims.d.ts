17078c82ad72bc44e13608e6bbe7fb82c3d2d4f3
Limbureuben <limbureuben4@gmail.com>
2025-06-01 18:48:09 +0300

    changes

--- src/vue-shims.d.ts
+++ src/vue-shims.d.ts
@@ -1,5 +1,24 @@
+// declare module '*.vue' {
+//   import { DefineComponent } from 'vue'
+//   const component: DefineComponent<{}, {}, any>
+//   export default component
+// }
+
+// src/vue-shims.d.ts
 declare module '*.vue' {
-  import { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
+    import { DefineComponent } from 'vue'
+    const component: DefineComponent<{}, {}, any>
+    export default component
+  }
+  
+  declare module '*.gql' {
+    import { DocumentNode } from 'graphql'
+    const value: DocumentNode
+    export default value
+  }
+  
+  declare module '*.graphql' {
+    import { DocumentNode } from 'graphql'
+    const value: DocumentNode
+    export default value
+  }
